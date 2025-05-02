9f84293d25d7ccd6e1e8708789acd13e5eb1d79b
Kari-Try <147976964+Kari-Try@users.noreply.github.com>
2025-05-03 00:00:46 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
+
+
     int N;
-    long long r, c;
+    int r, c;
     cin >> N >> r >> c;
 
-    long long ans = 0;
+    int ans = 0;
     for (int i = 0; i < N; i++) {
-        long long rb = (r >> i) & 1LL;
-        long long cb = (c >> i) & 1LL;
+        int rb = (r >> i) & 1LL;
+        int cb = (c >> i) & 1LL;
         ans |= (rb << (2 * i + 1));
         ans |= (cb << (2 * i));
     }
